145db31038ca16912e7cb9193cd8461535a1d0be
HuyHoangCh <tranhuyhoang159753@gmail.com>
2024-12-19 16:33:32 +0700

    Add files via upload

---
+++ scrips/baocao1.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+function submitFeedback() {
+	var name = document.getElementById('name'). value;
+	var feedback = document.getElementById('feedback').value;
+	
+	if (name === '') {
+		alert('Họ và tên không được bỏ trống');
+	} else if(feedback === ''){
+		alert('Nội dung góp ý không được bỏ trống');
+	} else{
+		var email = 'tranhuyhoang159753@gmail.com';
+		var subject = 'Feedback from ' + name;
+		var body = 'Họ và tên:' + name + '\nNội dung góp ý: ' + feedback;
+		window.location.href = 'mailto:' + email + '?subject=' +
+			encodeURIComponent(subject) + '&body= ' + encodeURIComponent(body);
+	}
+}
+function resetForm() {
+	document.getElementById('feedbackForm').reset();
+}
